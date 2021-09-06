f4067a0c9bd269a8667b020f56e83a4206f9fdf0
tomas-rln <tomas.ezequiel.rln@gmail.com>
2021-09-06 02:58:52 -0300

    styles

---
+++ src/components/Panels/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core'
+
+export const useStyles = makeStyles(theme => ({
+  container: {
+    border: '2px red solid',
+    borderRadius: 20,
+    marginTop: theme.spacing(2)
+  }
+}))
