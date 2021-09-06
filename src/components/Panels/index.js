806657695972b3650f16ba3e6442d48a07d54eff
tomas-rln <tomas.ezequiel.rln@gmail.com>
2021-09-06 02:58:39 -0300

    Add grid for style in tabs

--- src/components/Panels/index.js
+++ src/components/Panels/index.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Grid } from '@material-ui/core'
 import TabPanel from '../Tabpanel'
 import FeatureList from '../Features'
+import { useStyles } from './styles'
 
 const Panels = () => {
   const categories = useSelector(state => state.category.categories)
   const value = useSelector(state => state.category.index)
+  const classes = useStyles()
 
   // Here I need the assessments for each property.
 
   return (
-    categories.map((item, i) => {
-      return (
-          <TabPanel key={i} value={value} index={i}>
-              <FeatureList categoryId={item.id} />
-          </TabPanel>
-      )
-    })
+    <Grid xs={12} container item className={classes.container}>
+      {categories.map((item, i) => {
+        return (
+            <TabPanel key={i} value={value} index={i}>
+                <FeatureList categoryId={item.id} />
+            </TabPanel>
+        )
+      })}
+
+    </Grid>
 
   )
 }
